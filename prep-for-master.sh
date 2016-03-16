fa1d09daf73f1fa831e305d0ac0a8b3d768f919c
Matt Dailey <matthew.dailey1@gmail.com>
2016-03-16 13:49:13 -0400

    prep-for-master can also be used to just rename a merged branch

--- prep-for-master.sh
+++ prep-for-master.sh
@@ -2,7 +2,8 @@
 
 test=true
 branch=''
-action=''
+debug=false
+action='all'
 while [[ $# > 0 ]] ; do
     key="$1"
     case $key in
@@ -12,8 +13,11 @@ while [[ $# > 0 ]] ; do
         -b|--branch)
             branch="$2"
             ;;
-        *)
-            action="$key"
+        -d|--debug)
+            debug=true
+            ;;
+        -r|--rename)
+            action=rename
             ;;
     esac
     shift
@@ -27,33 +31,58 @@ fi
 squash=$branch-squash
 
 set -e
-set -x
-# make sure branch exists, fail if it doesn't
-git checkout $branch
+$debug && set -x
+function rebase_onto_master() {
+    # make sure branch exists, fail if it doesn't
+    git checkout $branch
 
-# make master up-to-date
-git checkout master
-git pull
+    # make master up-to-date
+    git checkout master
+    git pull
 
-# go to the branch, make a new branch for the squashed commits, then rebase onto master
-git checkout $branch
-git checkout -b $squash
+    # go to the branch, make a new branch for the squashed commits, then rebase onto master
+    git checkout $branch
+    git checkout -b $squash
 
-if $test; then
-    echo "Finished testing before rebase.  Check that everything is good, then perform"
-    echo "git rebase -i master"
-    exit 0
+    if $test; then
+        echo "Finished testing before rebase.  Check that everything is good, then perform"
+        echo "$0 --rebase"
+        return 0
+    fi
+    git rebase -i master
+}
+
+function merge_to_master() {
+    number_of_commits=$(git rev-list master..$squash --count)
+    if [ $number_of_commits -ne 1 ] ; then
+        echo "ERROR: Squashed branch ($squash) isn't small enough."
+        echo "       Should have only 1 commit but have $number_of_commits between master and this branch"
+        echo "       Fix with a manual 'git rebase -i master'"
+        exit 10
+    fi
+    git checkout master
+    git merge $squash
+    echo "Time to push me up to origin :)"
+    git diff --name-status origin/master
+}
+
+function rename_merged_branch() {
+    if [ "$branch" == "master" ] ; then
+        echo "Sorry, I won't rename the master branch"
+        return 20
+    elif $(echo "$branch" | grep '^merged' &> /dev/null) ; then
+        echo "Pretty sure branch $branch is already renamed, so not renaming it"
+        return 21
+    fi
+    git branch -m "$branch" "merged-$branch"
+}
+
+if [ "$action" == 'all' ] ; then
+    echo "Running the whole process"
+    rebase_onto_master
+    merge_to_master
+    rename_merged_branch
+elif [ "$action" == 'rename' ] ; then
+    echo "Just renaming branch"
+    rename_merged_branch
 fi
-git rebase -i master
-
-number_of_commits=$(git rev-list master..$squash --count)
-if [ $number_of_commits -ne 1 ] ; then
-    echo "ERROR: Squashed branch ($squash) isn't small enough."
-    echo "       Should have only 1 commit but have $number_of_commits between master and this branch"
-    echo "       Fix with a manual 'git rebase -i master'"
-    exit 10
-fi
-git checkout master
-git merge $squash
-echo "Time to push me up to origin :)"
-git diff --name-status origin/master
