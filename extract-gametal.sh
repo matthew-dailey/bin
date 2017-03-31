61b08dfbfef1714ca082176a024670f8e74c70d0
Matt Dailey <matthew.dailey1@gmail.com>
2017-03-31 14:24:42 -0400

    extract-gametal.sh has --delete option to delete after extraction

--- extract-gametal.sh
+++ extract-gametal.sh
@@ -3,6 +3,12 @@
 ## Extracts GaMetal audio from zip files into the Music directory
 ## music downloads from http://www.jonnyatma.com/gametal/news.html
 
+delete=''
+
+if [[ "$1" == "-d" ]] || [[ "$1" == "--delete" ]]; then
+  delete=true
+fi
+
 destination="$HOME/Music/GaMetal"
 mkdir -p "$destination"
 
@@ -10,5 +16,9 @@ for zipfile in ~/Downloads/GaMetal*; do
     # if there are no files, $zipfile will just be "~/Downloads/GaMetal*" which doesn't exist
     if [ -f "$zipfile" ] ; then
         unzip "$zipfile" -d "$destination"
+        if [[ "$delete" == "true" ]] ; then
+            echo "deleting $zipfile"
+            rm "$zipfile"
+        fi
     fi
 done
